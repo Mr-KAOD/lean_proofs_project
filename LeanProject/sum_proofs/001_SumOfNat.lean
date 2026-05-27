e766718f7c44079c2c3cb1f33962ddbe2ec0231a
mrkaod <k.a.o.d077@gmail.com>
2026-05-27 16:35:53 -0500

    Initial approach to Induction and Strong Induction

---
+++ LeanProject/sum_proofs/001_SumOfNat.lean
@@ -0,0 +1,19 @@
+import Mathlib
+
+def sumTo : Nat → Nat
+  | 0 => 0
+  | (n + 1) => sumTo n + (n + 1)
+
+theorem sumOfNat (n : Nat) :
+  2 * (sumTo n) = n * (n + 1) := by
+
+    induction n with
+      | zero => rfl
+
+      | succ k hyp =>
+        simp[sumTo]
+
+        rw[Nat.left_distrib]
+        rw[hyp]
+
+        ring
