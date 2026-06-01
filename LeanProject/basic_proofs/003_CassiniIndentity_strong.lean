5955c438212e7fb663a9142dbdf52a91c9a49d05
Matthew Liu <liumatthew2357@gmail.com>
2026-06-01 18:05:53 -0400

    P260601:M: Added Comments to Cassini Strongm

--- LeanProject/basic_proofs/003_CassiniIndentity_strong.lean
+++ LeanProject/basic_proofs/003_CassiniIndentity_strong.lean
@@ -15,12 +15,27 @@ theorem Cassini (n : ℕ) :
       · -- Base case: n = 1
         norm_num [F]
       · -- Induction step: n = k + 2
-        have ihk := ih k (by omega)
-
-        simp [F] at ihk
-        ring_nf at ihk
-
+        -- Completely unfold into F(1 + k) and F(k)
         simp [F]
+
+        -- Algebraic simplification that results in the following
+        -- F (1 + k) * F k - F (1 + k) ^ 2 + F k ^ 2 = -(-1) ^ k
         ring_nf
 
+        -- Induction Hypothesis
+        -- ih : ∀ m < k+2, Cassini(m)
+        -- Define ihk as the induction hypothesis for m = k
+          -- Then we can use it to rewrite the current goal
+        have ihk := ih k (by omega)
+
+        -- Completely unfold into F(1 + k) and F(k)
+        simp [F] at ihk
+        -- Algebraic simplification that results in the following
+        -- F (1 + k) * F k - F (1 + k) ^ 2 + F k ^ 2 = -(-1) ^ k
+        ring_nf at ihk
+
+        -- The induction hypothesis and the current goal are the same
+        -- so we can rewrite the current goal using the induction hypothesis
         exact ihk
+
+        -- Done
