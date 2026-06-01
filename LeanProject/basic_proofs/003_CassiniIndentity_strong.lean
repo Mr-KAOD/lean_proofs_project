160fe8c661caa1a298e4cfd1f5750e1fdf3fdf13
Matthew Liu <liumatthew2357@gmail.com>
2026-06-01 17:44:53 -0400

    P260601:M:Completed second version of Strong Induction Proof for Cassini's Identity

--- LeanProject/basic_proofs/003_CassiniIndentity_strong.lean
+++ LeanProject/basic_proofs/003_CassiniIndentity_strong.lean
@@ -1,92 +1,26 @@
 import Mathlib.Tactic
 
-def F : Nat → Int
+def F : ℕ → ℤ
   | 0 => 0
   | 1 => 1
-  | m + 1 => (F (m - 1)) + (F m)
+  | m + 2 => F (m + 1) + F (m)
 
-lemma unfold (k : Nat) (ok: k > 0):
-  F (k + 1) = (F (k - 1) + F k) := by
-
-  cases k with
-  | zero =>
-    contradiction
-  | succ j =>
-    simp [F]
-
-
-
-lemma F_sub_mid (n : Nat) (ok : n >= 1) :
-  F n = (F (n + 1)) - (F (n - 1)) := by
-    cases n with
-    | zero =>
-      contradiction
-    | succ k =>
-      simp [F]
-
-lemma F_sub_low (n : Nat) :
-  (F (n - 1)) = (F (n + 1)) - (F n) := by sorry
-
-lemma rearrange_sub (k : ℕ) (h : k ≥ 1) : (k - 1) + a = (k + a) - 1 := by
-  rw [Nat.sub_add_comm h]
-
-theorem Cassini (n : Nat) :
-  ((F n) * (F (n + 2)) - (F (n + 1)) ^ 2) = (-1) ^ (n + 1) := by
-
-    -- Strong induction on n
+theorem Cassini (n : ℕ) :
+  F (n + 2) * F n - F (n + 1) ^ 2 = (-1) ^ (n + 1) := by
     induction n using Nat.strong_induction_on with
     | h n ih =>
-
-      by_cases h : n = 0
-      ·
-        -- Base Case: n = 0
-        -- substitute n = 0 into the proposition
-        subst n
-        -- arithmetic verification
+      rcases n with _ | _ | k
+      · -- Base case: n = 0
         norm_num [F]
+      · -- Base case: n = 1
+        norm_num [F]
+      · -- Induction step: n = k + 2
+        have ihk := ih k (by omega)
 
-      ·
-        cases n with
-        | zero => contradiction
-        | succ k =>
-        have ih1 := ih (k - 1) (by omega)
-        rw [rearrange_sub k (by sorry)] at ih1
-        rw [rearrange_sub k (by sorry)] at ih1
-        norm_num at ih1
+        simp [F] at ihk
+        ring_nf at ihk
 
-        have hk1 : k + 1 >= 1 := by
-          omega
-        have hk0' : k > 0 := by
-          sorry
-        have hk1' : k + 1 > 0 := by
-          omega
-        have hk2' : k + 2 > 0 := by
-          omega
-
-        change F (k + 1) * F (k + 3) - F (k + 2) ^ 2 = (-1) ^ (k + 2)
-        -- Use the definition of F to simplify the goal
-        rw [unfold k hk0']
-        rw [unfold (k + 1) hk1']
-        rw [unfold (k + 2) hk2']
-        norm_num
-        ring_nf
-        rw [Nat.add_comm 1 k]
-        rw [Nat.add_comm 2 k]
-
-        nth_rewrite 2 [F_sub_mid (k + 1) hk1]
-
-        simp only [Nat.add_assoc, Nat.add_sub_cancel]
-        norm_num
-
-        ring_nf
-        rw [Nat.add_comm 1 k]
-        rw [Nat.add_comm 2 k]
-
-        rw [unfold (k + 1) hk1']
-        norm_num
-        nth_rewrite 2 [F_sub_low k]
+        simp [F]
         ring_nf
 
-        rw [Nat.add_comm 1 k]
-
-        rw [ih1]
+        exact ihk
